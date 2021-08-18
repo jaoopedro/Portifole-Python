56c6e2b4324130cfc1893749ffd9c3fb06c79310
joaopedro <jpmarcosnunes@gmail.com>
2021-08-17 22:38:23 -0300

    .

---
+++ exemplo07.py
@@ -0,0 +1,7 @@
+A = input("insira o valor de A")
+B = input("insira o valor B")
+C = input("insira o valor de C")
+
+Delta = float(B) * float(B) - 4 * float(A) * float(C)
+
+print("o delta é",Delta)
