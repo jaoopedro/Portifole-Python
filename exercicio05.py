4136d04c3499f2b89e8369dacabbf775b921f5e3
joaopedro <jpmarcosnunes@gmail.com>
2021-08-17 22:27:07 -0300

    .

--- exercicio05.py
+++ exercicio05.py
@@ -1,6 +1,6 @@
-nota1 = input("Digite sua nota")
-nota2 = input("Digite sua nota")
-nota3 = input("Digite sua nota")
+nota1 = input("Digite sua do primeiro bimestre")
+nota2 = input("Digite sua do  egundo bimestre")
+nota3 = input("Digite sua do terceiro bimestre")
 
 
 somatorio = float(nota1) + float(nota2) + float(nota3)
