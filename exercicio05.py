a7339afc6550747e960c10d54a771184a5dbadc5
joaopedro <jpmarcosnunes@gmail.com>
2021-08-17 22:21:21 -0300

    .

---
+++ exercicio05.py
@@ -0,0 +1,10 @@
+nota1 = input("Digite sua nota")
+nota2 = input("Digite sua nota")
+nota3 = input("Digite sua nota")
+
+
+somatorio = float(nota1) + float(nota2) + float(nota3)
+
+media = somatorio / 3
+
+print("sua media é", media)
