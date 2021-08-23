236dc05f45f7eddc58cab589594a75fbb3ce3774
joaopedro <jpmarcosnunes@gmail.com>
2021-08-23 10:44:55 -0300

    operações aritmetricas

--- exercicio05.py
+++ exercicio05.py
@@ -1,12 +1,12 @@
 # exercicio 3 do estudo dirigido (Operadores)
 
-nota1 = input("Digite sua do primeiro bimestre")
-nota2 = input("Digite sua do  egundo bimestre")
-nota3 = input("Digite sua do terceiro bimestre")
+nota1 = input("Digite sua do primeiro bimestre: ")
+nota2 = input("Digite sua do  egundo bimestre: ")
+nota3 = input("Digite sua do terceiro bimestre: ")
 
 
 somatorio = float(nota1) + float(nota2) + float(nota3)
 
 media = somatorio / 3
 
-print("sua media é", media)
+print("sua media é", round(media, 2))
