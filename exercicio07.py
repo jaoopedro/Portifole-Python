d89b394ccfffa1b85651643729bc65499b6561ed
joaopedro <jpmarcosnunes@gmail.com>
2021-08-23 10:50:53 -0300

    operações aritmetricas

--- exercicio07.py
+++ exercicio07.py
@@ -1,9 +1,11 @@
 # exercicio 7 do estudo dirigido (Operadores)
 
-A = input("insira o valor de A")
-B = input("insira o valor B")
-C = input("insira o valor de C")
+A = input("insira o valor de A: ")
+B = input("insira o valor B: ")
+C = input("insira o valor de C: ")
 
-Delta = float(B) * float(B) - 4 * float(A) * float(C)
+# Delta = float(B) * float(B) - 4 * float(A) * float(C)
+#               ou
+Delta = float(B) ** 2 - 4 * float(A) * float(C)
 
 print("o delta é",Delta)
