7af51ba539f526fc44d71d90126f6564b706f491
joaopedro <jpmarcosnunes@gmail.com>
2021-08-23 11:04:06 -0300

    operações aritmetricas

--- exercicio08.py
+++ exercicio08.py
@@ -3,7 +3,7 @@
 celsius = input("Digite o numero para a conversao -> ")
 
 
-C = float(celsius) * 9/ 5 + 32
+fahrenheit = float(celsius) * 9/ 5 + 32
 
 
-print("conversao",C)
+print("conversao",fahrenheit)
