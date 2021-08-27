39dfe831c6c4bae12569f7d5e74d658598c4a936
joaopedro <jpmarcosnunes@gmail.com>
2021-08-27 10:35:22 -0300

    Uso do end e do sep para criar separadores

---
+++ anotações.py
@@ -0,0 +1,15 @@
+# () se refere a uma função
+
+# print função de saída
+
+# função sep = '' "serve para por um separador"
+# print('joao', 'pedro', sep = '-')
+
+# função end = '' "serve para adicionar algo no final da variavel."
+# print('joao', end = '###')
+
+# fazendo um CPF
+# primeiro devemos tratar os numeros como strings
+
+print('824','176','555', sep='.',end='-')
+print('14')
