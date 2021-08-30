307cdd3e72dd03231fc892e26c32064667829129
joaopedro <jpmarcosnunes@gmail.com>
2021-08-30 11:37:00 -0300

    exemplo do exercicio 6 de uma forma mais limpa

---
+++ exemplo06.py
@@ -0,0 +1,8 @@
+salariob = float(input("digite o salario: "))
+
+desconto = salariob * 0.08
+salarioli = salariob - desconto
+
+print("salario bruto: R$",salariob)
+print("desconto de 8%: R$", desconto)
+print("salario liquido: R$",salarioli)
