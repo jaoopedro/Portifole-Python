74f78e643eccf1cdb8191bab2363b7ced74ce6bf
joaopedro <jpmarcosnunes@gmail.com>
2021-08-30 11:21:11 -0300

    Logica dos operadores de atribuição

---
+++ programa08.py
@@ -0,0 +1,27 @@
+num = 4
+
+som = sub = mul = divr = divi = mod = exp = 10
+
+som +=num #som = som + num
+
+sub -= num # -> sub = sub - num
+
+mul *= num # -> mul = mul * num
+
+divr /= num # -> divr = divr / num
+
+divi //= num # -> divi = divi // num
+
+mod %= num # -> mod = mod % num
+
+exp **= num # -> exp = exp ** num
+
+print("adição", som)
+print("subitração", sub)
+print("multiplicação", mul)
+print("divisão real", divr)
+print("divisão inteira", divi)
+print("Resto da divisão", mod)
+print("exponenciação", exp)
+
+print("o numero é", num)
