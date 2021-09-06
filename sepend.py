7e3f9dbe5e7c8ed550b776412768744e2848bd21
joaopedro <jpmarcosnunes@gmail.com>
2021-09-06 10:43:12 -0300

    atualização dos separadores forma mais facil para separar cpf

--- sepend.py
+++ sepend.py
@@ -7,6 +7,7 @@
 
 # fazendo um CPF
 # primeiro devemos tratar os numeros como strings
+# ou simplifica criando uma variavel -> cpff = cpf[0:3] + "." + cpf[3:6] + "." + cpf[6:9] + "-" + cpf[9:11]
 
 print('824','176','555', sep='.',end='-')
 print('14')
