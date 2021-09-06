17a7876bdb486abc922a952b3d66169f4e18eec4
joaopedro <jpmarcosnunes@gmail.com>
2021-09-06 11:00:43 -0300

    Uso do strip para remover espaços do inicio e do final uso do lower para por a letra em minuscula uso do upper para letra maiuscula

---
+++ programa20.py
@@ -0,0 +1,5 @@
+palavra = input("digite: ")
+
+print("sem espaços: ---", palavra.strip(),"---") # "---" so foi usado para provar que tira espaço
+print("palavra em letras minuscula",palavra.lower())
+print("palavra em letras MAIUSCULAS",palavra.upper())
