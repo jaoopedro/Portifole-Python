ed48df60ddf6fd714f3a3988d8f55a8bc6070bf8
joaopedro <jpmarcosnunes@gmail.com>
2021-09-06 11:10:02 -0300

    Uso do strip para remover espaços do inicio e do final Uso do lower para por a letra em minuscula Uso do upper para letra maiuscula Uso do replace para substitiuir dentro da string

--- programa20.py
+++ programa20.py
@@ -1,5 +1,13 @@
 palavra = input("digite: ")
 
 print("sem espaços: ---", palavra.strip(),"---") # "---" so foi usado para provar que tira espaço
+
+# Metodo lower() e upper()
 print("palavra em letras minuscula",palavra.lower())
 print("palavra em letras MAIUSCULAS",palavra.upper())
+
+# Metodo replace() substitiu um caractere / trecho por outra dentro da string
+# Sintaxe variavel.replace()
+
+print("trocando a por X",palavra.replace("a","x"))
+#                  (quem vai ser substituido/ quem vai substitiuir)
