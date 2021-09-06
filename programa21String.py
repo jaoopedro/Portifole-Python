06aeb856a4277ea456ef33b40dc6b3b260c7dea9
joaopedro <jpmarcosnunes@gmail.com>
2021-09-06 11:44:01 -0300

    Uso do format

---
+++ programa21String.py
@@ -0,0 +1,6 @@
+# -> Metodo format serve para montar uma string minsturando texto e variavel.
+# Sintaxe: variavel.format()
+
+mensagem = "o(a) aluno(a) {0} obteve media {1} e esta {2}"
+
+print(mensagem.format("joao",7,"aprovado"))
