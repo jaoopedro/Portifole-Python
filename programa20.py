1e737c485619ce5e4a87ab6cb7f26b46bf673ec6
joaopedro <jpmarcosnunes@gmail.com>
2021-09-06 11:30:25 -0300

    Uso do strip para remover espaços do inicio e do final Uso do lower para por a letra em minuscula Uso do upper para letra maiuscula Uso do replace para substitiuir dentro da string serve tambem para remover espaço no meio Uso do split para dividir strings

--- programa20.py
+++ programa20.py
@@ -6,6 +6,7 @@ print("sem espaços: ---", palavra.strip(),"---") # "---" so foi usado para prov
 print("palavra em letras minuscula",palavra.lower())
 print("palavra em letras MAIUSCULAS",palavra.upper())
 
+
 # Metodo replace() substitiu um caractere / trecho por outra dentro da string
 # Sintaxe variavel.replace()
 
@@ -14,3 +15,9 @@ print("trocando a por X",palavra.replace("a","x"))
 
 # uso do replace para remover espaços no meio
 print("sem espaço no meio",palavra.replace(" ",""))
+
+
+# Metodo split() divide a string em substrings, ou seja, divide em palavras/ um divisor/ divide oq estiver no split
+# Sintaxe variavel.split("caratere_dellimitador")
+
+print("Texto dividido", palavra.split(" "))
