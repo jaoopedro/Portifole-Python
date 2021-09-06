f52c0f18728452991f2e2cd49eae749d9696d3a2
joaopedro <jpmarcosnunes@gmail.com>
2021-09-06 10:42:17 -0300

    manipulação de strings e o uso do len

--- programa19string.py
+++ programa19string.py
@@ -1,7 +1,11 @@
 # Para saber qual o caracterie de acordo com a posição a sintaxe é variavel[e um numero de acordo]
 # para pegar um trecho inteiro Sintaxe variavel[posição inicial: posição final]
-#                                                 inclusiva     : exclusive
+#                                              [   inclusiva     : exclusive  ]
+
 # USO DO len(variavel) serve para da a quantidade de caracteres
+
+# saber a letra sem saber a palavra que digitaram variavel[qtd - 1]
+
 palavra = input("Digite uma palavra: ")
 
 
@@ -12,4 +16,8 @@ print("A terceira letra é:", palavra[2])
 print("trecho  entre a posição 2 é 8", palavra[2:8])
 
 qtd = len(palavra)
-print("A palavra", palavra, "tem", qtd,"caracteres")
+#print("A palavra", palavra, "tem", qtd,"caracteres")
+print("A ultima é",palavra[qtd - 1])
+
+
+
