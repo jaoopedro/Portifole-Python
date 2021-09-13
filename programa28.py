afebd096a39f9735bbf088eff14a447908b8e694
joaopedro <jpmarcosnunes@gmail.com>
2021-09-13 10:14:44 -0300

    Exercicios do uso de if e else

--- programa28.py
+++ programa28.py
@@ -4,7 +4,7 @@ nota1 = float(input("Digite sua nota: "))
 nota2 = float(input("Digite sua nota: "))
 nota3 = float(input("Digite sua nota: "))
 
-media = nota1 + nota2 + nota3 / 3
+media = (nota1 + nota2 + nota3) / 3
 if media >= 6:
     print("Aprovado")
 # else:
