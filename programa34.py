b29a6bec7bf6ef4a17b766ae7aa05146a072851f
joaopedro <jpmarcosnunes@gmail.com>
2021-09-13 11:55:02 -0300

    Exercicios do uso de if e else para saber pontuação de nota

---
+++ programa34.py
@@ -0,0 +1,15 @@
+# Exercicio 9 do estudo dirigido (estrutura de seleção if e else )
+
+nota1 = float(input("Digite sua nota: "))
+nota2 = float(input("Digite sua nota: "))
+nota3 = float(input("Digite sua nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+if media >= 6:
+    print("Aprovado",round(media,2))
+    print("Passou com",round(media - 6,2),"acima da media" )
+else:
+    print("Reprovado")
+    print("Faltam",round(6 - media,2),"para atingir a media")
+
+print("* FIM DE PROGRAMA *")
