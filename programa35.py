eb1c22fe91829051c05d429b52176b356d6ec07e
joaopedro <jpmarcosnunes@gmail.com>
2021-09-13 12:16:42 -0300

    Exercicios do uso de if e else para saber se o numero é impar ou par

---
+++ programa35.py
@@ -0,0 +1,10 @@
+# exercicio 12 do estudo dirigido (estrutura de seleção if e else )
+
+num = int(input("Digite seu numero: "))
+
+div = num % 2
+
+if div == 0:
+    print("O numero é Par")
+else:
+    print("O numero é impar")
