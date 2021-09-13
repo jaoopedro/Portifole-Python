7221480b63f89700ec86727c294b59aac1f55b6e
joaopedro <jpmarcosnunes@gmail.com>
2021-09-13 10:13:35 -0300

    Exercicios do uso de if e else

---
+++ programa27.py
@@ -0,0 +1,8 @@
+# Exercicio 1 do estudo dirigido (estrutura de seleção if e else )
+
+idade = int(input("Digite a idade: "))
+
+if idade >=18:
+    print("maior de idade")
+# else:
+    #print("menor de idade")
---
+++ programa28.py
@@ -0,0 +1,11 @@
+# Exercicio 2 do estudo dirigido (estrutura de seleção if e else )
+
+nota1 = float(input("Digite sua nota: "))
+nota2 = float(input("Digite sua nota: "))
+nota3 = float(input("Digite sua nota: "))
+
+soma = nota1 + nota2 + nota3 / 3
+if soma >= 6:
+    print("Aprovado")
+# else:
+   # print("Reprovado")
