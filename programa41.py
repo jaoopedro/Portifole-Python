f038848c48d1a11de5e83950c679d8cae24ba85d
joaopedro <jpmarcosnunes@gmail.com>
2021-09-20 11:54:52 -0300

    uso do Operador logico and

---
+++ programa41.py
@@ -0,0 +1,12 @@
+from datetime import date
+print('Data de hoje',date.today())
+print()
+data = int(input("Digite o ano do seu nascimento: "))
+sexo = input("<M/F>: ")
+
+idade = date.today().year - data
+
+if idade >= 18 and sexo =='M':
+    print("Serviço militar Obrigatorio")
+else:
+    print("Insento do Serviço Militar")
