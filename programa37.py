2c73abadf25ed3a308297450dbdbd18b153f2dc9
joaopedro <jpmarcosnunes@gmail.com>
2021-09-20 10:04:18 -0300

    uso do elif para mais de uma condição

---
+++ programa37.py
@@ -0,0 +1,11 @@
+# exercicio 15 do estudo dirigido (estrutura de seleção if e else )
+
+num = int(input("Digite seu numero: "))
+
+
+if num >0:
+    print(num,"é positivo")
+elif num < 0:
+    print(num,"é negativo")
+else:
+    print(num,"é nulo")
