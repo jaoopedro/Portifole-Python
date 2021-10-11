e65270af8b01f0708b0b2e8a19a9066149ee80ab
joaopedro <jpmarcosnunes@gmail.com>
2021-10-11 11:01:10 -0300

    programa com o for para contar ate o numero que voce escolher

--- programa48.py
+++ programa48.py
@@ -1,6 +1,8 @@
 # exercicio 2 do estudo dirigido sobre estrutura de repetição for
 
-n = int(input("Digite o numero"))
+n = int(input("Digite o numero: "))
 
 for i in range(1,n+1):
     print("Execução",i)
+
+print("* FIM DE PROGRAMA *")
