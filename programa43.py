e77c538db01b87fabf1cb7ec011dd8791491f014
joaopedro <jpmarcosnunes@gmail.com>
2021-10-11 09:48:03 -0300

    joguinho de perguntas com as estruturas if,elif e else

--- programa43.py
+++ programa43.py
@@ -4,7 +4,7 @@ import math
 n = float(input("Digite um número: "))
 
 
-if not(n < 0): #quando o not for falso ele é verdadeiro
+if not(n < 0): #quando o not for falso ele executa
     rq = math.sqrt(n)
     saida = 'A raiz quadrada {0} é {1}'
     print(saida.format(n,round(rq, 2)))
