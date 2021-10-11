5f316523bced9e147bfc267fbc30fcc53085c0de
joaopedro <jpmarcosnunes@gmail.com>
2021-10-11 10:36:37 -0300

    uso da estrutura de repetição for

---
+++ programa45.py
@@ -0,0 +1,6 @@
+# exercicio 1 do estudo dirigido sobre estrutura de repetição for
+
+for i in range(5):
+    n = int(input("Digite um numero: "))
+    saida = 'O triplo de {0} é {1}'
+    print(saida.format(n, n*3))
