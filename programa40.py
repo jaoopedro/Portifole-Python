b7dc222e947e4e4c0d4382c22090c93fe7eef186
joaopedro <jpmarcosnunes@gmail.com>
2021-10-14 09:32:40 -0300

    programa atualizado de consulta de credito

--- programa40.py
+++ programa40.py
@@ -4,7 +4,7 @@ nome = input("nome do cliente: ")
 sal = float(input("Digite o salario Bruto: R$ "))
 cpf = input("Tem Restrinção no cpf <S/N>: ")
 
-if sal >1100 and cpf =='N':
+if sal >1100 and cpf.upper() =='N':
     print("cliente",nome,'teve o credito Aprovado')
     print("Boas Compras")
 else:
