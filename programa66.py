5bad702e25b2b9dc9081b8c3abc63da0424c1c69
joaopedro <jpmarcosnunes@gmail.com>
2021-11-01 09:57:10 -0300

    uso do operador de repetição while para fazer conta de notas

---
+++ programa66.py
@@ -0,0 +1,21 @@
+# exercício 4 do estudo dirigido sobre estrutura de repetição while
+
+soma = 0
+
+nota = float(input('Digite a nota: '))
+
+qtd = 0
+
+while nota >= 0 and nota <= 10:
+
+    soma += nota
+
+    qtd = qtd + 1
+
+    nota = float(input('Digite a nota: '))
+
+if qtd == 0:
+    print('Não foi digitada nenhuma nota válida!')
+
+else:
+    print('A média é', round(soma/qtd, 2))
