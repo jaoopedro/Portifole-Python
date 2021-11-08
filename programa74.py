869e937d5a8fe46f0aa8d0537099c17f741e514a
joaopedro <jpmarcosnunes@gmail.com>
2021-11-08 10:17:04 -0300

    uso do Arrays(listas) no python, agora utilizando o input para inserir

---
+++ programa74.py
@@ -0,0 +1,19 @@
+# Exercicio 3 do estudo dirigido de Arrays
+
+num = []
+
+num.append(input("Digite o numero que deseja inserir na lista: "))
+num.append(input("Digite o numero que deseja inserir na lista: "))
+num.append(input("Digite o numero que deseja inserir na lista: "))
+num.append(input("Digite o numero que deseja inserir na lista: "))
+num.append(input("Digite o numero que deseja inserir na lista: "))
+
+print(num)
+
+print('***** POSIÇÃO POR POSIÇÃO ***** ')
+
+print('posição 0:' ,num[0])
+print('posição 1:' ,num[1])
+print('posição 2:' ,num[2])
+print('posição 3:' ,num[3])
+print('posição 3:' ,num[4])
