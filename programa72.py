ff19be89b6d86bb5244d05d2183f34b4095e5065
joaopedro <jpmarcosnunes@gmail.com>
2021-11-08 10:01:21 -0300

    uso do Arrays(listas) no python

---
+++ programa72.py
@@ -0,0 +1,16 @@
+nomes = []
+
+nomes.append('joao')
+nomes.append('jose')
+nomes.append('lucas')
+nomes.append('ingrid')
+
+
+print(nomes)
+
+print('***** POSIÇÃO POR POSIÇÃO ***** ')
+
+print('posição 0:' ,nomes[0])
+print('posição 1:' ,nomes[1])
+print('posição 2:' ,nomes[2])
+print('posição 3:' ,nomes[3])
