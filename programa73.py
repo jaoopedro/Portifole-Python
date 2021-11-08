c7468d569bca826e172543b63c26143c7f45a2a5
joaopedro <jpmarcosnunes@gmail.com>
2021-11-08 10:10:35 -0300

    uso do Arrays(listas) no python

---
+++ programa73.py
@@ -0,0 +1,17 @@
+# Exercicio 2 do estudo dirigido de Arrays
+numeros = []
+
+numeros.append(18)
+numeros.append(5)
+numeros.append(26)
+numeros.append(38)
+#.append serve para inserir na lista
+
+print(numeros)
+
+print('***** POSIÇÃO POR POSIÇÃO ***** ')
+
+print('posição 0:' ,numeros[0])
+print('posição 1:' ,numeros[1])
+print('posição 2:' ,numeros[2])
+print('posição 3:' ,numeros[3])
