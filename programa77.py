d90211ed9f6becf0055120b4437c87544f256746
joao Pedro <jaoopedro.dev@gmail.com>
2021-11-11 21:15:11 -0300

    uso do Arrays(listas) no python, agora utilizando o input para inserir e utilizando o For para contar os numeros da lista na  ordem inversa

---
+++ programa77.py
@@ -0,0 +1,12 @@
+nomes =  []
+
+nomes.append('jose')
+nomes.append('maria')
+nomes.append('pedro')
+nomes.append('lucas')
+
+nomes.insert(2,'mel')
+print(nomes)
+
+nomes.remove('maria')
+print(nomes)
