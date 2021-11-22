161cc51978470e7f6aae9b9f0ac88895058d2479
joao Pedro <jaoopedro.dev@gmail.com>
2021-11-22 09:28:38 -0300

    uso dA FUNÇÃO def para criar funções

---
+++ programa78.py
@@ -0,0 +1,12 @@
+# Exercicio 1 do estudo dirigido de Funçoes
+
+def calcularTriplo():
+    n = int(input('Digite um numero: '))
+    msg = 'O triplo de {0} é {1}'
+    print(msg.format(n, n*3))
+
+print('*** INICIO DO PROGRAMA ***')
+
+calcularTriplo()
+
+print('*** FIM DO PROGRAMA ***')
