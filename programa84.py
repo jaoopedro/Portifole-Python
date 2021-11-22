5b809d5aba387568959ffa1b108b29c5315664d1
joao Pedro <jaoopedro.dev@gmail.com>
2021-11-22 11:37:33 -0300

    programa81 utilizando o parametro no def

---
+++ programa84.py
@@ -0,0 +1,55 @@
+# Exercicio 4 do estudo dirigido de Funçoes
+
+def calcularMedia(n1,n2):
+
+    print('A media é', (n1 + n2)/2)
+
+def calcularDifereca(n1,n2):
+
+    if n1 > n2:
+        print('A diferença é',n1 - n2)
+    else:
+        print('A diferença é',n2 - n1)
+
+
+def calcularProduto(n1,n2):
+
+    print('O Produto é', n1*n2)
+
+def calcularDivisao(n1,n2):
+
+    print('A divisão é',n1/n2)
+
+print('***** INÍCIO DO PROGRAMA *****')
+
+n1 = float(input('Digite um numero: '))
+n2 = float(input('Digite outro numero: '))
+
+print('As operações são:')
+
+print('(1) Média Aritmética')
+
+print('(2) Diferença do maior pelo menor')
+
+print('(3) Produto')
+
+print('(4) Divisão')
+
+opcao = int(input('Digite a opção: '))
+
+if opcao ==1:
+    print('Media Aritmetrica')
+    calcularMedia(n1,n2)
+elif opcao ==2:
+    print('Diferença')
+    calcularDifereca(n1,n2)
+elif opcao ==3:
+    print('Produto')
+    calcularProduto(n1,n2)
+elif opcao ==4:
+    print('Divisão')
+    calcularDivisao(n1,n2)
+else:
+    print('OPÇÃO INVALIDA!!!')
+
+print('***** FIM DO PROGRAMA *****')
